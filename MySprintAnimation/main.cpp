3f1a44bb39e7f19754c63da59446222176f188b9
zhouleigang <meixi@live.cn>
2021-03-19 11:56:10 +0800

    update project

--- MySprintAnimation/main.cpp
+++ MySprintAnimation/main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char *argv[])
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QCoreApplication a(argc, argv);
 
-#if 0
+#if 1
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/mysprint.qml")));
 #else
--- QtProject.pro
+++ QtProject.pro
@@ -1,6 +1,8 @@
 TEMPLATE = subdirs
 
+src_quicktest.subdir = $$PWD/quicktest
 src_mysprintanimation.subdir = $$PWD/mysprintanimation
 
 SUBDIRS += \
+    src_quicktest \
     src_mysprintanimation
